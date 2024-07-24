80b0f83f344a82137c7e285c278d4744a8fee44f
kkanazaw <new.frontier2006@gmail.com>
2024-07-24 17:55:18 +0900

    refs #1 セッションを使うように修正

--- lib/devise/fireauth/strategies/firebase_authenticatable.rb
+++ lib/devise/fireauth/strategies/firebase_authenticatable.rb
@@ -3,25 +3,20 @@
 module Devise
   module Strategies
     class FirebaseAuthenticatable < Authenticatable
-      def valid?
-        !token.nil?
-      end
-
-      # Don't use Session
-      def store?
-        false
-      end
       #
       # For an example check : https://github.com/plataformatec/devise/blob/master/lib/devise/strategies/database_authenticatable.rb
       #
       # Method called by warden to authenticate a resource.
       #
       def authenticate!
+        auth_params = authentication_hash
+        return fail! unless auth_params[Fireauth.token_key]
+
         #
         # mapping.to is a wrapper over the resource model
         #
         # Treat the password as idToken
-        resource = mapping.to.firebase_authentication(token)
+        resource = mapping.to.firebase_authentication(auth_params)
 
         return fail! unless resource
 
@@ -36,29 +31,6 @@ module Devise
         if validate(resource)
           success!(resource)
         end
-      end
-
-      def authenticate
-        resource = mapping.to.firebase_authentication(token)
-        # If authenticated, stop immediately - or continue
-        resource ? success!(resource) : fail(resource)
-      end
-
-      private
-
-      def token
-        token_from_headers || token_from_params
-      end
-
-      def token_from_headers
-        authorization_header = env["HTTP_AUTHORIZATION"]
-        return unless authorization_header
-        type, token = authorization_header.split
-        type =~ /Bearer/i ? token : nil
-      end
-
-      def token_from_params
-        params.dig Devise::Fireauth.token_key
       end
     end
   end
